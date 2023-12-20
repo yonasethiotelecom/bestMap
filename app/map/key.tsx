594af07b531834afba20bb0067b0a6b54d73e9ce
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2023-12-20 09:43:28 +0300

    first4 commit

--- app/map/key.tsx
+++ app/map/key.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 function key() {
   return (
-    <div style={{ color: '#8DC63F'}} className=' z-50 fixed  bottom-0 w-full flex justify-between items-center  bg-white bg-opacity-50 p-2 '>
+    <div style={{ color: '#8DC63F'}} className=' z-30 fixed  bottom-0 w-full flex justify-between items-center  bg-white bg-opacity-50 p-2 '>
          <div className='flex space-x-2'>
       
         <div className='  rounded-full w-6 h-6'  style={{ backgroundColor: 'rgba(135,206 , 255, 0.2)'}}></div>
--- app/map/map.tsx
+++ app/map/map.tsx
@@ -7,6 +7,18 @@ import "leaflet/dist/leaflet.css";
 import Image from 'next/image'
 
 
+import { Button } from "@/components/ui/button"
+import Modal from 'react-modal';
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
 
 import L, {LatLngTuple } from 'leaflet';
 
@@ -14,13 +26,13 @@ import { getDistance } from 'geolib';
 import{sites,Site} from '@/lib/data'
 import{SearchField} from './searchField'
 import Swal from 'sweetalert2';
-import { Compass, TimerReset } from 'lucide-react';
+import { Compass, Search, TimerReset } from 'lucide-react';
 import Link from 'next/link';
+import { MapBoxProvider } from 'leaflet-geosearch';
 
 const zoom = 15
-
-
 const radius=250;
+
 
 function MapPlaceholder() {
   return (
@@ -66,7 +78,8 @@ const nearbyCoordinates:Site[] = sites;
 function LocationMarker() {
   const [position, setPosition] = useState<LatLngTuple | null>(null);
   const [nerestDistance ,setNerestDistance]=useState<number>(0)
-  
+  const [networkType, setNetworkType] = useState<string>('Unknown');
+  const [signalStrength, setSignalStrength] = useState<number | null>(null);
 
 const [flage ,setFlage] =useState(true)
  
@@ -88,14 +101,13 @@ console.log("abezakew"+nerestDistance )
 if(flage==true){
       Swal.fire({
         title: nerestDistance  == 0
-          ?  '5G network available in this area'
-          : 'No 5G network coverage available in this area',
+          ?  `5G network available in this area    Network Type: ${networkType} Signal Strength: ${signalStrength}` 
+          : `No 5G network coverage available in this area   Network Type: ${networkType} Signal Strength: ${signalStrength}`,
         icon: 'info',
         showConfirmButton: false,
         timer: 10000, // 10 seconds
         showCloseButton: true,
-    
-
+  
         
       });
 
@@ -112,12 +124,24 @@ if(flage==true){
 
 
   useEffect(() => {
+    if ('connection' in navigator) {
+      const network = (navigator as any).connection.effectiveType;
+      setNetworkType(network);
+    }
+
+    if ('getSignalStrength' in navigator) {
+      (navigator as any).getSignalStrength().then((signal: number) => {
+        setSignalStrength(signal);
+      });
+    }
+
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
           setPosition([latitude, longitude]);
           setNerestDistance(findNearestDistance([latitude, longitude]));
+         
           map.flyTo([latitude, longitude], map.getZoom());
        
           
@@ -179,9 +203,38 @@ if(flage==true){
 
 export default function Map() {
   const fillBlueOptions = { fillColor: "#0072BC" };
-  const center: LatLngTuple = [9.03306, 38.76487];
-
+  const [center, setCenter] = useState<LatLngTuple>([9.03306, 38.76487]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 const [zoomLevel, setZoomLevel] = useState(15);
+const [flag,setFlag]=useState(false)
+const [modalIsOpen, setIsOpen] = useState(false);
+
+
+const [latitude, setLatitude] = useState('');
+const [longitude, setLongitude] = useState('');
+
+const [position2, setPosition2] = useState<LatLngTuple | null>(null);
+
+
+const handleFindPosition = async (map:any,searchTerm:any) => {
+
+  console.log("yonas mulugeta")
+  
+
+  const locationResponse = await fetch(
+    `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(searchTerm)}`
+  );
+  const locationData = await locationResponse.json();
+
+  console.log('Location Object:', locationData);
+
+
+
+map.setView([parseFloat(locationData[0].lat), parseFloat(locationData[0].lon)],15)
+  setPosition2([parseFloat(locationData[0].lat), parseFloat(locationData[0].lon)]);
+  
+};
 
 
 
@@ -219,15 +272,16 @@ function DisplayPosition({ map }:{map:any}) {
 
 
 
+
+
+
+
 function FindPosition({ map }:{map:any}) {
   const [position, setPosition] = useState(() => map.getCenter())
 
   const onClick = useCallback(() => {
 
     map.locate();
-      
-      
-
   
   }, [map])
 
@@ -273,26 +327,177 @@ const [map, setMap] = useState(null)
 
 
 
+
+
+
+const handleInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+  const query = e.target.value;
+  setSearchTerm(query);
+
+
+  try {
+    const response = await fetch(
+      `https://nominatim.openstreetmap.org/search?format=json&city=${query}&country=Ethiopia&state=Addis Ababa`
+    );
+    const data = await response.json();
+    setSearchResults(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  
+
+  }}
+
+  const handleOptionClick =  (index:number,map:any) => {
+
+   
+    // Filter the result based on ID
+    const filtered :any= searchResults.filter((result:any) => result.id === index);
+   if( filtered.length > 0){
+  
+
+    map.flyTo([parseFloat(filtered.lat), parseFloat(filtered.lon)], 13);
+   }
+  
+  
+  };
+
+
+
+ 
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  //  subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      backgroundColor: 'white',
+ 
+  
+    },
+  };
+
   return (
 <div className=' flex flex-col '>
 
-<div>
-
-  
+<div> 
 
 <div className=' flex  justify-between  items-center    w-full'  style={{backgroundColor:'#FFFFFF'}}>
         
-        <div  style={{  margin: '10px', padding: '10px' }} >
+        <div  style={{  margin: '10px', padding: '10px' }} className=' max-sm:hidden' >
         <Link href="/">
             <Image src='/photoDagm3.png' alt='5G' width={163} height={42} />
             </Link>
-        </div>
+       </div>
 
         <div style={{ color: '#8DC63F', margin: '10px', textAlign: 'center' }}>
 
     <div className='flex  justify-center  items-center gap-4  max-sm:flex-col '>
+      <div className='flex  justify-between items-center w-full '>
+      <div  style={{  margin: '10px', padding: '10px' }} className=' sm:hidden' >
+        <Link href="/">
+            <Image src='/photoDagm3.png' alt='5G' width={163} height={42} />
+            </Link>
+       </div>
     <h1  className=' text-[1.8rem] max-md:text-sm'  style={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}>Check Network coverage</h1>
+    </div>
     <div className='flex gap-4'>
+
+
+
+    <div>
+<button className="btn" onClick={() => {
+  const modal = document.getElementById('my_modal_4');
+  if (modal) {
+    //@ts-ignore
+    modal.showModal();
+  }
+}}> <div className="flex items-center">
+<span className='whitespace-nowrap'>Advanced Search</span>
+<Search size={28} color="#8DC63F" strokeWidth={2.75} />
+</div></button>
+<dialog id="my_modal_4" className="modal">
+<form method="dialog" className="modal-backdrop">
+    <button>close</button>
+  </form>
+  <div className="modal-box  w-11/12 max-w-5xl   ">
+
+
+
+  <form method="dialog">
+      {/* if there is a button in form, it will close the modal */}
+      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+    </form>
+    
+
+  <div className='flex flex-col   justify-start items-center mx-auto   gap-4 '>
+  <label htmlFor="citySearch" style={{ fontSize: '1.25rem', fontWeight: 'bold', marginBottom: '0.5rem' }}>
+    Select a city in Ethiopia (Addis Ababa):
+  </label>
+
+  <input
+    type="text"
+    placeholder="Search..."
+    value={searchTerm}
+    className="input input-bordered input-success w-full max-w-xs  sticky top-0"
+    onChange={handleInputChange}
+  />
+
+<div className="h-48">
+ <ul className="m-2 text-red-500 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+  {map ? (
+    <>
+      {searchResults.map((result: any) => (
+
+        
+        <li
+          key={result.place_id}
+          value={result.display_name}
+          className="border-b-2 border-gray-300 py-2 px-3 cursor-pointer text-black hover:text-red-500 active:text-blue-600"
+          onClick={() => handleFindPosition(map, result.display_name)}
+        >
+         
+          <form method="dialog">
+      
+      <button className="btn"> {result.display_name}</button>
+    </form>
+       
+        </li>
+      ))}
+    </>
+  ) : null}
+</ul>
+</div>  
+</div>  
+    <div className="modal-action ">
+    {/*   <form method="dialog">
+      
+        <button className="btn">Close</button>
+      </form>
+ */}
+    </div>
+  </div>
+</dialog>
+</div>
+
+
+  
+
     {map ? <FindPosition map={map} /> : null}
     {map ? <DisplayPosition map={map} /> : null}
     </div>
@@ -309,14 +514,20 @@ const [map, setMap] = useState(null)
 
 
 
-
 </div>
 
 
+{flag?(<>
 
 
+
+
+
+
+
+
+</>):(<>
     <div >
-
      
 
       <MapContainer
@@ -344,7 +555,17 @@ const [map, setMap] = useState(null)
           ]}
         />
  <SearchField/>
-
+ {position2 && <Marker position={position2} icon={
+              new L.Icon({
+              iconUrl: '/icongg.png', 
+             iconRetinaUrl: '/icongg.png',
+                iconSize: [30, 27],
+                iconAnchor: [12.5, 41],
+                popupAnchor: [0, -41],
+                shadowUrl: MarkerShadow.src,
+                shadowSize: [41, 41],
+              })
+            } />}
 
 
 < MyComponent/>
@@ -651,6 +872,9 @@ const [map, setMap] = useState(null)
       
       </MapContainer>
       </div>
+      </>)
+}
+
     </div>
   );
 }
